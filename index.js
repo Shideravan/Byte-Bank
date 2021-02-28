ec240f4189ae90c312345e58dfd228eea23e52fc
Shideravan <spannopan@yahoo.com.br>
2021-02-28 01:16:57 -0300

    Commit final

--- index.js
+++ index.js
@@ -7,8 +7,10 @@ const diretor = new Diretor("Marcio", 10000, 123456);
 diretor.cadastrarSenha("123123");
 const gerente = new Gerente("Thais", 5000, 854321);
 gerente.cadastrarSenha("321321");
+const cliente = new Cliente("Lais", 121212, "456");
 
-const estaLogadoDir = SistemaAutenticacao.login(diretor, "123123");
-const estaLogadoGer = SistemaAutenticacao.login(gerente, "321321");
-console.log(estaLogadoDir);
-console.log(estaLogadoGer);
+const diretorEstaLogado = SistemaAutenticacao.login(diretor, "123123");
+const gerenteEstaLogado = SistemaAutenticacao.login(gerente, "321321");
+const clienteEstaLogado = SistemaAutenticacao.login(cliente, "456");
+
+console.log(diretorEstaLogado, gerenteEstaLogado, clienteEstaLogado);
